83919d96fd8f7d59dd299df22430c2bed7d5015f
Digi <haider.317.98@gmail.com>
2022-11-02 18:17:41 +0100

    Buttons Work!

--- commands/alllineups.js
+++ commands/alllineups.js
@@ -1,5 +1,4 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -31,6 +30,69 @@ module.exports = {
         );
         await interaction.reply({ content: 'Which map?', ephemeral: true, components: [maps1] });  
         
-        
+        const collector = interaction.channel.createMessageComponentCollector({ componentType: ComponentType.Button, time: 15000 });
+
+            const maps2 = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('Haven')
+                    .setLabel('Haven')
+                    .setStyle(ButtonStyle.Primary),
+                new ButtonBuilder()
+                    .setCustomId('Icebox')
+                    .setLabel('Icebox')
+                    .setStyle(ButtonStyle.Primary),
+                new ButtonBuilder()
+                    .setCustomId('Pearl')
+                    .setLabel('Pearl')
+                    .setStyle(ButtonStyle.Primary),
+                new ButtonBuilder()
+                    .setCustomId('Split')
+                    .setLabel('Split')
+                    .setStyle(ButtonStyle.Primary)
+                    .setEmoji('1036245600590643241'),
+                new ButtonBuilder()
+                    .setCustomId('Back')
+                    .setLabel('Back')
+                    .setStyle(ButtonStyle.Secondary),
+                );
+            
+            collector.on('collect', async i => {
+                if (i.customId === 'More') {
+                    await i.update({ content: 'Which Map?', ephemeral: true, components: [maps2] });
+                }
+                if (i.customId === 'Back') {
+                    await i.update({ content: 'Which Map?', ephemeral: true, components: [maps2] });
+                }
+                if (i.customId === 'Ascent') {
+                    await i.update({ content: 'This is Ascent', ephemeral: true, components: [] });
+                }
+                if (i.customId === 'Bind') {
+                    await i.reply({ content: 'This is Bind', ephemeral: true });
+                }
+                if (i.customId === 'Breeze') {
+                    await i.reply({ content: 'This is Breeze', ephemeral: true });
+                }
+                if (i.customId === 'Fracture') {
+                    await i.reply({ content: 'This is Fracture', ephemeral: true });
+                }
+                if (i.customId === 'Haven') {
+                    await i.reply({ content: 'This is Haven', ephemeral: true });
+                }
+                if (i.customId === 'Icebox') {
+                    await i.reply({ content: 'This is Icebox', ephemeral: true });
+                }
+                if (i.customId === 'Pearl') {
+                    await i.reply({ content: 'This is Pearl', ephemeral: true });
+                }
+                if (i.customId === 'Split') {
+                    await i.reply({ content: 'This is Split', ephemeral: true });
+                }
+            });
+            
+
+            collector.on('end', collected => {
+                console.log(`Collected ${collected.size} interactions.`);
+            });
     },
 }
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -19,41 +19,6 @@ module.exports = {
             }
         } else if (interaction.isButton()) {
             console.log(interaction);
-
-            const collector = interaction.channel.createMessageComponentCollector({ componentType: ComponentType.Button, time: 15000 });
-
-            const maps2 = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setCustomId('Haven')
-                    .setLabel('Haven')
-                    .setStyle(ButtonStyle.Primary),
-                new ButtonBuilder()
-                    .setCustomId('Icebox')
-                    .setLabel('Icebox')
-                    .setStyle(ButtonStyle.Primary),
-                new ButtonBuilder()
-                    .setCustomId('Pearl')
-                    .setLabel('Pearl')
-                    .setStyle(ButtonStyle.Primary),
-                new ButtonBuilder()
-                    .setCustomId('Split')
-                    .setLabel('Split')
-                    .setStyle(ButtonStyle.Primary)
-                    .setEmoji('1036245600590643241'),
-                new ButtonBuilder()
-                    .setCustomId('Back')
-                    .setLabel('Back')
-                    .setStyle(ButtonStyle.Secondary),
-                );
-            
-            collector.on('collect', async i => {
-                await i.update({ content: 'Which Map?', ephemeral: true, components: [maps2] });
-            });
-            
-            collector.on('end', collected => {
-                console.log(`Collected ${collected.size} interactions.`);
-            });
         }       
 	},
 };
